968174e989e521e42beb2cae53814bf5e1b2d1d9
difeng pan <difeng pan>
2016-10-26 07:36:09 +0800

    fdasfdsa

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -37,7 +37,7 @@ return array(
     /* 数据库配置 */ 
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'xh_db1', // 数据库名
+    'DB_NAME'   => 'xh_db', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'Pa0d31f219',  // 密码
     'DB_PORT'   => '3306', // 端口 
