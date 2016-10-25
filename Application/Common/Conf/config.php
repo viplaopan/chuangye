af0e72f023fe9ffacebe40073cb266e24aae620d
difeng pan <difeng pan>
2016-10-25 23:20:16 +0800

    yun

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -37,10 +37,10 @@ return array(
     /* 数据库配置 */ 
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'xh_db', // 数据库名
+    'DB_NAME'   => 'xh_db1', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'Pa0d31f219',  // 密码
-    'DB_PORT'   => '33060', // 端口
+    'DB_PORT'   => '3306', // 端口 
     'DB_PREFIX' => 'xh_', // 数据库表前缀
     
     'LOAD_EXT_CONFIG' => 'router',
