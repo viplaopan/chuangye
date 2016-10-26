9b711433ce1a9824e0103945d54acb8a4c817828
difeng pan <difeng pan>
2016-10-26 08:45:15 +0800

    test

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,95 +4,47 @@
 // +----------------------------------------------------------------------
 // | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.thinkphp.cn>
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
 /**
- * 前台配置文件
- * 所有除开系统级别的前台配置
+ * 系统配文件
+ * 所有系统级别的配置
  */
 return array(
+    /* 模块相关配置 */
+    'AUTOLOAD_NAMESPACE' => array('Addons' => ONETHINK_ADDON_PATH), //扩展模块列表
+    'DEFAULT_MODULE'     => 'Main',
+    'MODULE_DENY_LIST'   => array('Common','User','Admin','Install'),
+    //'MODULE_ALLOW_LIST'  => array('Home','Admin'),
 
-    // 预先加载的标签库
-    'TAGLIB_PRE_LOAD'     =>    'OT\\TagLib\\Article,OT\\TagLib\\Think',
-        
-    /* 主题设置 */
-    'DEFAULT_THEME' =>  'default',  // 默认模板主题名称
+    /* 系统数据加密设置 */
+    'DATA_AUTH_KEY' => 'qy8@xhX1(6T#"*>-,~.A2K[]n}l|<DsRYbctZ4e{', //默认数据加密KEY
 
-    /* 数据缓存设置 */
-    'DATA_CACHE_PREFIX' => 'onethink_', // 缓存前缀
-    'DATA_CACHE_TYPE'   => 'File', // 数据缓存类型
+    /* 用户相关设置 */
+    'USER_MAX_CACHE'     => 1000, //最大缓存用户数
+    'USER_ADMINISTRATOR' => 1, //管理员用户ID
 
-    /* 文件上传相关配置 */
-    'DOWNLOAD_UPLOAD' => array(
-        'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 5*1024*1024, //上传的文件大小限制 (0-不做限制)
-        'exts'     => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', //允许上传的文件后缀
-        'autoSub'  => true, //自动子目录保存文件
-        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-        'rootPath' => './Uploads/Download/', //保存根路径
-        'savePath' => '', //保存路径
-        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-        'saveExt'  => '', //文件保存后缀，空则使用原后缀
-        'replace'  => false, //存在同名是否覆盖
-        'hash'     => true, //是否生成hash编码
-        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
-    ), //下载模型上传配置（文件上传类配置）
+    /* URL配置 */
+    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
+    'URL_MODEL'            => 2, //URL模式
+    'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
-    /* 编辑器图片上传相关配置 */
-    'EDITOR_UPLOAD' => array(
-        'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 2*1024*1024, //上传的文件大小限制 (0-不做限制)
-        'exts'     => 'jpg,gif,png,jpeg', //允许上传的文件后缀
-        'autoSub'  => true, //自动子目录保存文件
-        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-        'rootPath' => './Uploads/Editor/', //保存根路径
-        'savePath' => '', //保存路径
-        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-        'saveExt'  => '', //文件保存后缀，空则使用原后缀
-        'replace'  => false, //存在同名是否覆盖
-        'hash'     => true, //是否生成hash编码
-        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
-    ),
+    /* 全局过滤配置 */
+    'DEFAULT_FILTER' => '', //全局过滤函数
 
-    /* 模板相关配置 */
-    'TMPL_PARSE_STRING' => array(
-        '__STATIC__' => __ROOT__ . '/Public/static',
-        '__ADDONS__' => __ROOT__ . '/Public/' . MODULE_NAME . '/Addons',
-        '__IMG__'    => __ROOT__ . '/Public/' . MODULE_NAME . '/images',
-        '__CSS__'    => __ROOT__ . '/Public/' . MODULE_NAME . '/css',
-        '__JS__'     => __ROOT__ . '/Public/' . MODULE_NAME . '/js',
-    ),
+    /* 数据库配置 */ 
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_NAME'   => 'xh_db', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => 'Pa0d31f219',  // 密码
+    'DB_PORT'   => '3306', // 端口 
+    'DB_PREFIX' => 'xh_', // 数据库表前缀
+    
+    'LOAD_EXT_CONFIG' => 'router',
 
-    /* SESSION 和 COOKIE 配置 */
-    'SESSION_PREFIX' => 'onethink_home', //session前缀
-    'COOKIE_PREFIX'  => 'onethink_home_', // Cookie前缀 避免冲突
-
-    /**
-     * 附件相关配置
-     * 附件是规划在插件中的，所以附件的配置暂时写到这里
-     * 后期会移动到数据库进行管理
-     */
-    'ATTACHMENT_DEFAULT' => array(
-        'is_upload'     => true,
-        'allow_type'    => '0,1,2', //允许的附件类型 (0-目录，1-外链，2-文件)
-        'driver'        => 'Local', //上传驱动
-        'driver_config' => null, //驱动配置
-    ), //附件默认配置
-
-    'ATTACHMENT_UPLOAD' => array(
-        'mimes'    => '', //允许上传的文件MiMe类型
-        'maxSize'  => 5*1024*1024, //上传的文件大小限制 (0-不做限制)
-        'exts'     => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', //允许上传的文件后缀
-        'autoSub'  => true, //自动子目录保存文件
-        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-        'rootPath' => './Uploads/Attachment/', //保存根路径
-        'savePath' => '', //保存路径
-        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-        'saveExt'  => '', //文件保存后缀，空则使用原后缀
-        'replace'  => false, //存在同名是否覆盖
-        'hash'     => true, //是否生成hash编码
-        'callback' => false, //检测文件是否存在回调函数，如果存在返回文件信息数组
-    ), //附件上传配置（文件上传类配置）
-
+    /* 文档模型配置 (文档模型核心配置，请勿更改) */
+    'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
 );
